f50feda975c5650eca6f731299bbb564e5d02dc3
Dan Jones <hello@danjones.dev>
2025-12-31 17:19:52 +0100

    chore(nvim): remove nvim-tree

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -57,14 +57,6 @@ require("lazy").setup({
 			end,
 		},
 		{
-			"nvim-tree/nvim-tree.lua",
-			lazy = false,
-			dependencies = { "nvim-tree/nvim-web-devicons" },
-			config = function()
-				require("nvim-tree").setup({})
-			end,
-		},
-		{
 			"mason-org/mason.nvim",
 			lazy = false,
 			opts = {},
