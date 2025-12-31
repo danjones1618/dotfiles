8b3b462276e8fd7674a85571a3b9a60af333a6f6
Dan Jones <hello@danjones.dev>
2025-12-31 16:29:46 +0100

    feat(nvim): add html formatting

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -1,3 +1,4 @@
+local web_formatting = { "biome-organize-iumports", "biome-check", "biome" }
 return {
 	"stevearc/conform.nvim",
 	opts = {
@@ -6,6 +7,13 @@ return {
 			rust = { "rustfmt", lsp_format = "fallback" },
 			lua = { "stylua" },
 			toml = { "taplo" },
+			javascript = web_formatting,
+			javascriptreact = web_formatting,
+			typescript = web_formatting,
+			typescriptreact = web_formatting,
+			json = web_formatting,
+			css = web_formatting,
+			html = web_formatting,
 		},
 		format_on_save = {
 			timeout_ms = 500,
