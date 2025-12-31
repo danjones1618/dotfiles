1d50db0e72eb29af9a9582f50d5d3389bdaf0a03
Dan Jones <hello@danjones.dev>
2025-12-31 16:54:44 +0100

    feat(nvim): adjust markdown textwidth to 100

---
+++ nvim/ftplugin/markdown.lua
@@ -0,0 +1,4 @@
+vim.opt_local.textwidth = 100
+vim.opt_local.formatoptions:append({ "t", "q" }) -- wrap & allow gq
+vim.opt_local.formatexpr = "" -- user built-in formatter
+vim.opt_local.colorcolumn = "+1"
